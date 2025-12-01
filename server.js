205b0fb13790bf497aff3609b65bb2bc643a6b22
michaelpme <michael.p.mekonnen@gmail.com>
2025-12-01 11:11:38 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,89 @@
+const express= require("express");
+const app = express();
+const PORT= 3001
+
+let phonebook=[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json());
+
+app.get("/api/persons",(request,response)=>{
+    response.json(phonebook)   
+})
+
+// Try to fix the string if time allows
+app.get("/info",(request,response)=>{
+
+    response.send(`Phonebook has info for ${phonebook.length} people` + "\n" + `${new Date()}`)
+})
+
+app.get("/api/persons/:id",(request,response)=>{
+    id=request.params.id
+    console.log(request)
+    console.log("test")
+    requestedObj= phonebook.find(item => item.id===id);
+    if(requestedObj){
+        response.json(requestedObj)
+    }else{
+        response.status(204).end() 
+    }
+})
+
+
+app.delete("/api/persons/:id",(request,response)=>{
+    id=request.params.id;
+    phonebook= phonebook.filter(item => item.id!==id)
+    response.status(202).end()
+})
+
+app.post("/api/persons/",(request,response)=>{
+    const req = request.body;
+    let listName= phonebook.map(item => item.name)
+    if(!req.name){
+      return response.status(400).json({
+            error:"Name is required"
+        })
+
+    }else if(!req.number){
+        return response.status(400).json({
+            error:"Number is required"
+        })
+
+    }else if(listName.includes(req.name)){
+        return response.status(404).json({
+            error:"Name must be unique"
+        })
+    }
+    else{
+        phonebook.push({
+            "id": (Math.floor(Math.random()*10000)),
+            "name": request.body.name, 
+            "number": request.body.number
+        })
+        response.status(202).end()
+    }
+})
+
+app.listen(PORT,(err)=>{
+    console.log(`server is running on port ${PORT}`)
+}) 
